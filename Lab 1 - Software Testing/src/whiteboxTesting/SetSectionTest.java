0a2e364d1b0d89d4f0aa69ac6cf28adec7606187
brode <brode@192.168.0.5>
2016-11-18 18:49:04 +0100

    # WARNING: head commit changed in the meantime
    first commit

--- Lab 1 - Software Testing/src/whiteboxTesting/SetSectionTest.java
+++ Lab 1 - Software Testing/src/whiteboxTesting/SetSectionTest.java
@@ -1,6 +1,5 @@
 package whiteboxTesting;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -10,13 +9,15 @@ public class SetSectionTest {
 	private Set set;
 	private Set deletionSet;
 
-	@Before public void setUp() {
+	@Before
+	public void setUp() {
 		set = new Set();
 		deletionSet = new Set();
 	}
 
-  // Statement and branch coverage
-	@Test public void testDeletion() {
+	// Statement and branch coverage
+	@Test
+	public void testDeletion() {
 		set.insert(5);
 		deletionSet.insert(5);
 		set.section(deletionSet);
@@ -24,7 +25,8 @@ public class SetSectionTest {
 		assertTrue(set.toArray().length == 0);
 	}
 
-	@Test public void testNoDeletion() {
+	@Test
+	public void testNoDeletion() {
 		set.insert(2);
 		deletionSet.insert(3);
 		set.section(deletionSet);
@@ -32,19 +34,29 @@ public class SetSectionTest {
 		assertTrue(set.toArray().length == 1);
 	}
 
-	@Test public void testDeletionMany() {
+	@Test
+	public void testDeletionMany() {
 		set.insert(1);
 		set.insert(2);
 		set.insert(3);
+		set.insert(4);
 		set.insert(5);
-		
+		set.insert(6);
+
 		deletionSet.insert(1);
 		deletionSet.insert(2);
-		deletionSet.insert(5);
-		set.section(deletionSet);
-		assertTrue(set.member(3));
-		assertFalse(set.member(5));
-		assertTrue(set.toArray().length == 1);
-	}
+		deletionSet.insert(3);
+		deletionSet.insert(4);
+		deletionSet.insert(6);
 
+		set.section(deletionSet);
+
+		assertFalse(set.member(1));
+		assertFalse(set.member(2));
+		assertFalse(set.member(3));
+		assertFalse(set.member(4));
+		assertTrue(set.member(5));
+		assertFalse(set.member(6));
+
+	}
 }
